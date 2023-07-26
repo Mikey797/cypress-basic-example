cf39a46899ac9167834d4ad20e5214acb30d8e78
Mihan Menuka <m.menuka@gmail.com>
2023-07-26 15:09:03 +0530

    modify cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -7,3 +7,16 @@ module.exports = defineConfig({
     },
   },
 });
+
+
+module.exports = (on, config) => {
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    console.log(launchOptions.args);
+
+    if (browser.name === 'chrome' || browser.name === 'edge' || browser.name === 'firefox') {
+      launchOptions.args.push('--disable-gpu');
+    }
+
+    return launchOptions;
+  });
+};
